8e0e2b20ee75eb864d21a038645dda9b8ed80d4b
ashishmodi27 <ashishmodi27@gmail.com>
2017-05-13 19:07:28 +0530

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MultiFaceRec
+{
+    public partial class FrmPrincipal : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        //Declararation of all variables, vectors and haarcascades
+        Image<Bgr, Byte> currentFrame;
+        Capture grabber;
+        HaarCascade face;
+        HaarCascade eye;
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            //Load haarcascades for face detection
+            face = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                //Load of previus trainned faces and labels for each image
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string LoadFaces;
+
+                for (int tf = 1; tf < NumLabels + 1; tf++)
+                {
+                    LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.ToString());
+                MessageBox.Show("Nothing in binary database, please add at least a face(Simply train the prototype with the Add Face Button).", "Triained faces load", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Initialize the capture device
+            grabber = new Capture();
+            grabber.QueryFrame();
+            //Initialize the FrameGraber event
+            Application.Idle += new EventHandler(FrameGrabber);
+            button1.Enabled = false;
+        }
+
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                //Trained face counter
+                ContTrain = ContTrain + 1;
+
+                //Get a gray frame from capture device
+                gray = grabber.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                //Face Detector
+                MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+                face,
+                1.2,
+                10,
+                Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                new Size(20, 20));
+
+                //Action for each element detected
+                foreach (MCvAvgComp f in facesDetected[0])
+                {
+                    TrainedFace = currentFrame.Copy(f.rect).Convert<Gray, byte>();
+                    break;
+                }
+
+                //resize face detected image for force to compare the same size with the 
+                //test image with cubic interpolation type method
+                TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox1.Text);
+
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Ashish Modi\Documents\modime.mdf;Integrated Security=True;Connect Timeout=30");
+                SqlCommand cmd;
+                con.Open();
+
+                cmd = new SqlCommand("insert into test2 (name,pic) values ('" + textBox1.Text + "','" + TrainedFace + "' )", con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("New user is added Successfully .");
+                con.Close();
+                this.Refresh();
+
+                //Show face added in gray scale
+                imageBox1.Image = TrainedFace;
+
+                //Write the number of triained faces in a file text for further load
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.ToArray().Length.ToString() + "%");
+
+                //Write the labels of triained faces in a file text for further load
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels.ToArray()[i - 1] + "%");
+
+                }
+
+                MessageBox.Show(textBox1.Text + "´s face detected and added :)", "Training OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Enable the face detection first", "Training Fail", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            label3.Text = "0";
+            //label4.Text = "";
+            NamePersons.Add("");
+
+
+            //Get the current frame form capture device
+            currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+            //Convert it to Grayscale
+            gray = currentFrame.Convert<Gray, Byte>();
+
+            //Face Detector
+            MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+          face,
+          1.2,
+          10,
+          Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+          new Size(20, 20));
+
+            //Action for each element detected
+            foreach (MCvAvgComp f in facesDetected[0])
+            {
+                t = t + 1;
+                result = currentFrame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                //draw the face detected in the 0th (gray) channel with blue color
+                currentFrame.Draw(f.rect, new Bgr(Color.Red), 2);
+                
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    //TermCriteria for face recognition with numbers of trained images like maxIteration
+                    MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+
+                    //Eigen face recognizer
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(
+                       trainingImages.ToArray(),
+                       labels.ToArray(),
+                       3000,
+                       ref termCrit);
+
+                    name = recognizer.Recognize(result);
+
+                    //Draw the label for each face detected and recognized
+                    currentFrame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.LightGreen));
+
+                }
+
+                NamePersons[t - 1] = name;
+                NamePersons.Add("");
+                //Set the number of faces detected on the scene
+                label3.Text = facesDetected[0].Length.ToString();
+            }
+            t = 0;
+
+            //Names concatenation of persons recognized
+            for (int nnn = 0; nnn < facesDetected[0].Length; nnn++)
+            {
+                names = names + NamePersons[nnn] + " ";
+            }
+            if (names != "")
+            {
+                SaveDetails(this.names);
+            }
+            //Show the faces procesed and recognized
+            imageBoxFrameGrabber.Image = currentFrame;
+            {
+                label4.Text = names;
+                //SaveDetails();
+            }
+            //SaveDetails();
+            names = "";
+            //Clear the list(vector) of names
+            NamePersons.Clear();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void SaveDetails(string names)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Ashish Modi\Documents\modime.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand cmd;
+            con.Open();
+
+            cmd = new SqlCommand("insert into modiji (name,time) values ('" + names + "','" + DateTime.Now + "' )", con);
+            //cmd = new SqlCommand("update modiji set status='present' where name=('" + names + "') ", con);
+            cmd.ExecuteNonQuery();
+            // MessageBox.Show("New user is added Successfully .");
+            cmd = new SqlCommand("update modiji set status='present' where name=('" + names + "') ", con);
+            cmd.ExecuteNonQuery();
+            //MessageBox.Show("Entry is added Successfully .");
+            cmd = new SqlCommand("delete from modiji where name=''", con);
+            cmd.ExecuteNonQuery();
+            //MessageBox.Show("deletion is added Successfully .");
+            con.Close();
+            this.Refresh();
+            System.Threading.Thread.Sleep(5000);
+
+            con.Close();
+
+            //catch (Exception ex)
+            //{
+            //    cmd = new SqlCommand("insert into modi (name,time) values ('" + names + "','" + DateTime.Now + "' )", con);
+            //    cmd.ExecuteNonQuery();
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+
+        }
+
+    }
+}
